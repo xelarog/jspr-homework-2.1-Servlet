bf863c635701af7bc028ae78a88d1937d34d87f0
Александр Горшков <stollprog@mail.ru>
2022-10-20 20:03:03 +0300

    make atomic for countId

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 // Stub
 public class PostRepository {
     private static volatile long countId;
+//    private static volatile AtomicLong countId;
 
     private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
 
@@ -34,6 +35,7 @@ public class PostRepository {
 
         long id = post.getId();
         if (id == 0) {
+     //       long idNext = countId.incrementAndGet();
             long idNext = ++countId;
             post.setId(idNext);
             posts.put(idNext, post);
