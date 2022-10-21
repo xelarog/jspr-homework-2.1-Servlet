a16ff789b8177597031a9f12721264b6cddc8df7
Александр Горшков <stollprog@mail.ru>
2022-10-21 21:46:24 +0300

    fix3

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 // Stub
 public class PostRepository {
-    private static AtomicLong countId = new AtomicLong();
+    private AtomicLong countId = new AtomicLong();
 
     private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
 
